d4274e01b6dee6e6cdde92e5d8b0527ed08d0fd2
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-09-17 17:49:04 +0530

    Added new style

--- app.js
+++ app.js
@@ -31,4 +31,31 @@ async function curratedPhotos() {
   });
 }
 
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application / json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+
+  data.photos.forEach((photo) => {
+    const gallaryImg = document.createElement("div");
+    gallaryImg.classList.add("gallary-img");
+    gallaryImg.innerHTML = `
+        <div class = 'gallary-info'
+            <p>${photo.photographer}</p>
+            <a href = ${photo.src.original}>Download</a>
+        </div>
+        <img src = ${photo.src.large}></img>
+    `;
+    gallary.appendChild(gallaryImg);
+  });
+}
+
 curratedPhotos();
