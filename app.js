9f96bbc7392622a42292fbfb11ec8260cc301432
Rudolph Pereira <rudolph.pereira93@gmail.com>
2020-09-22 08:22:05 +0530

    Added finishing style and loadMore function

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const auth = "563492ad6f91700001000001650cc6c0622a404e9833fa455c7b91a9";
 const gallary = document.querySelector(".gallary");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
 let searchValue;
 let fetchLink;
+let currentSearch;
+let page = 1;
 
 // Add Event Listeners
-
-searchInput.addEventListener("input", loadMore);
+searchInput.addEventListener("input", inputUpdate);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener("click", loadMore);
 
-function loadMore(e) {
+function inputUpdate(e) {
   searchValue = e.target.value;
 }
 
@@ -53,6 +57,17 @@ async function curratedPhotos() {
 async function searchPhotos(query) {
   clear();
   fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
+  generatePhotos(data);
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
   const data = await fetchApi(fetchLink);
   generatePhotos(data);
 }
